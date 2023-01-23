755451a8da2ff8a9d5a64f17be67d44b8673850f
Italo <108245748+italo-ho@users.noreply.github.com>
2023-01-23 19:46:37 -0300

    Carregando o arquivo 'imagens.js' do projeto

---
+++ imagens.js
@@ -0,0 +1,22 @@
+//Imagens e soms do Jogo:
+
+let imagemEstrada;
+let imagemAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3];
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav");
+}
